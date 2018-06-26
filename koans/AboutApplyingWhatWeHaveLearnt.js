448afdbed112681343e2be87e43d332e21b9168b
davydhong <davydhong@gmail.com>
2018-06-26 09:49:36 -0700

    Completed 4 required koans

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -1,6 +1,7 @@
-var _; // globals
+var _; //globals
 
 describe("About Applying What We Have Learnt", function() {
+
   var products;
 
   beforeEach(function () { 
@@ -16,6 +17,7 @@ describe("About Applying What We Have Learnt", function() {
   /*********************************************************************************/
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
+
     var i,j,hasMushrooms, productsICanEat = [];
 
     for (i = 0; i < products.length; i+=1) {
@@ -34,6 +36,7 @@ describe("About Applying What We Have Learnt", function() {
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
+
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
@@ -44,8 +47,8 @@ describe("About Applying What We Have Learnt", function() {
   /*********************************************************************************/
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
+    
     var sum = 0;
-
     for(var i=1; i<1000; i+=1) {
       if (i % 3 === 0 || i % 5 === 0) {
         sum += i;
@@ -56,6 +59,7 @@ describe("About Applying What We Have Learnt", function() {
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
+
     var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
 
     expect(233168).toBe(FILL_ME_IN);
